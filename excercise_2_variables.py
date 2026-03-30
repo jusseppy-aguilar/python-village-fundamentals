9b9dacb194923eb425a9cd0c7e9bc892dd2e5b07
Jusseppy Aguilar <jusseppy.aguilar@gmail.com>
2026-03-30 13:49:56 -0500

    Add solution for Rosalind Variables and Arithmetic.

--- excercise_2_variables.py
+++ excercise_2_variables.py
@@ -14,10 +14,7 @@ def main() -> None:
     Designed to receive data directly injected from the standard input (sys.stdin),
     making it ideal for automated pipelines and high-performance computing (HPC).
     """
-    # Reads all incoming standard input, splits by whitespace, and maps to integers
     a, b = map(int, sys.stdin.read().split())
-    
-    # Executes the calculation and prints the final result
     print(sum_of_squares(a, b))
 
 if __name__ == "__main__":
